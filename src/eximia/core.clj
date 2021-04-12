167aaa2bd3869672dfbb8f1f0ed36a32ce1f0c8e
Pauli Jaakkola <pauli.jaakkola@iki.fi>
2021-04-12 16:02:59 +0300

    Add writing

--- src/eximia/core.clj
+++ src/eximia/core.clj
@@ -1,6 +1,7 @@
 (ns eximia.core
-  (:import [javax.xml.stream XMLInputFactory XMLStreamReader XMLStreamConstants]
-           [java.io Reader Writer InputStream StringReader]
+  (:refer-clojure :exclude [read])
+  (:import [javax.xml.stream XMLInputFactory XMLStreamReader XMLStreamWriter XMLStreamConstants XMLOutputFactory]
+           [java.io Reader Writer InputStream OutputStream StringReader StringWriter]
            [javax.xml.namespace QName]))
 
 ;;;; # QName Support
@@ -9,6 +10,18 @@
   (.write out "#qname[")
   (.write out (.toString qname))
   (.write out (int \])))
+
+;;;; # Output Conversions
+
+(defprotocol ^:private ToStreamWriter
+  (-stream-writer ^XMLStreamWriter [self factory]))
+
+(extend-protocol ToStreamWriter
+  Writer
+  (-stream-writer [self factory] (.createXMLStreamWriter ^XMLOutputFactory factory self))
+
+  OutputStream
+  (-stream-writer [self factory] (.createXMLStreamWriter ^XMLOutputFactory factory self)))
 
 ;;;; # Input Conversions
 
@@ -25,6 +38,32 @@
   String
   (-stream-reader [self factory] (.createXMLStreamReader ^XMLInputFactory factory (StringReader. self))))
 
+;;;; # Writing
+
+(defprotocol WriteXML
+  (-write [self ^XMLStreamWriter out]))
+
+(defrecord Element [tag attrs content]
+  WriteXML
+  (-write [_ out]
+    (let [^XMLStreamWriter out out
+          ^QName tag tag]
+      (.writeStartElement out (.getPrefix tag) (.getLocalPart tag) (.getNamespaceURI tag))
+      (reduce-kv (fn [^XMLStreamWriter out ^QName k v]
+                   (.writeAttribute out (.getPrefix k) (.getLocalPart k) (.getNamespaceURI k) v))
+                 out attrs)
+      (reduce (fn [out child] (-write child out) out) out content)
+      (.writeEndElement out))))
+
+(extend-type String
+  WriteXML
+  (-write [s out] (.writeCharacters ^XMLStreamWriter out s)))
+
+(defn- write-document [tree ^XMLStreamWriter out]
+  (.writeStartDocument out)
+  (-write tree out)
+  (.writeEndDocument out))
+
 ;;;; # Parsing
 
 (defmacro ^:private eval-case [e & clauses]
@@ -37,8 +76,6 @@
                         2 (let [[pat expr] clause]
                             [(if (seq? pat) (map eval pat) (eval pat))
                              expr])))))))
-
-(defrecord Element [tag attrs content])
 
 (defn- parse-tokens [^XMLStreamReader input]
   (letfn [(skip-prolog [^XMLStreamReader input]
@@ -100,13 +137,31 @@
 
 ;;;; # API
 
-(def ^:private ^XMLInputFactory default-factory
+(def ^:private ^XMLInputFactory default-input-factory
   (doto (XMLInputFactory/newFactory)
     (.setProperty XMLInputFactory/IS_SUPPORTING_EXTERNAL_ENTITIES false)
     (.setProperty XMLInputFactory/IS_COALESCING true)))
 
-(defn parse
-  ([input] (parse input default-factory))
+(defn read
+  ([input] (read input default-input-factory))
   ([input xml-input-factory]
    (with-open [input (-stream-reader input xml-input-factory)]
      (parse-tokens input))))
+
+(def ^:private ^XMLOutputFactory default-output-factory
+  (doto (XMLOutputFactory/newFactory)
+    (.setProperty XMLOutputFactory/IS_REPAIRING_NAMESPACES true)))
+
+(defn write
+  ([tree out] (write tree out default-output-factory))
+  ([tree out xml-output-factory]
+   (with-open [out (-stream-writer out xml-output-factory)]
+     (write-document tree out))))
+
+(defn write-str
+  ([tree] (write-str tree default-output-factory))
+  ([tree xml-output-factory]
+   (with-open [sout (StringWriter.)
+               out (-stream-writer sout xml-output-factory)]
+     (write-document tree out)
+     (.toString sout))))
