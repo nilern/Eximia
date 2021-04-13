c2b4d857904490126e3dc51d85a4439d54c746f3
Pauli Jaakkola <pauli.jaakkola@iki.fi>
2021-04-13 19:57:54 +0300

    Add test

--- test/eximia/core_test.clj
+++ test/eximia/core_test.clj
@@ -1,12 +1,16 @@
 (ns eximia.core-test
   (:require [eximia.core :as e]
-            [clojure.test :refer :all]
-            [clojure.test.check.generators :as gen])
+            [clojure.test.check.generators :as gen]
+            [clojure.test.check.properties :refer [for-all]]
+            [clojure.test.check.clojure-test :refer [defspec]]
+            [clojure.string :as str])
   (:import [javax.xml XMLConstants]))
 
 ;;;; # Generators
 
-(def characters-gen gen/string)
+(def content-char-gen (gen/such-that (fn [^Character c] (< (.indexOf "<&" (int c)) 0)) gen/char))
+
+(def characters-gen (gen/fmap str/join (gen/vector content-char-gen)))
 
 (def cdata-gen (gen/fmap e/cdata characters-gen))
 
@@ -17,15 +21,18 @@
             data (gen/one-of [gen/string (gen/return nil)])]
     e/processing-instruction ))
 
+;; HACK:
+(def name-gen (gen/fmap name gen/symbol))
+
 (def qname-gen
-  (gen/let [local-name gen/string
-            namespace-uri (gen/one-of [gen/string (gen/return XMLConstants/NULL_NS_URI)])
-            prefix (gen/one-of [gen/string (gen/return XMLConstants/DEFAULT_NS_PREFIX)])]
+  (gen/let [local-name name-gen
+            namespace-uri (gen/one-of [name-gen (gen/return XMLConstants/NULL_NS_URI)])
+            prefix (gen/one-of [name-gen (gen/return XMLConstants/DEFAULT_NS_PREFIX)])]
     (e/qname namespace-uri local-name prefix)))
 
 (def attrs-gen (gen/map qname-gen gen/string))
 
-(def non-element-content-gen (gen/one-of [characters-gen cdata-gen comment-gen pi-gen]))
+(def non-element-content-gen (gen/one-of [characters-gen cdata-gen pi-gen comment-gen]))
 
 (defn element-of-gen [element-gen]
   (gen/let [tag qname-gen
@@ -35,3 +42,9 @@
 
 (def element-gen
   (element-of-gen (gen/recursive-gen element-of-gen non-element-content-gen)))
+
+;;;; # Tests
+
+(defspec write-read
+  (for-all [element element-gen]
+    (= element (e/read-str (e/write-str element)))))
