4be898ca672fa0d61bc8fdee1033468232d93b61
Pauli Jaakkola <pauli.jaakkola@iki.fi>
2021-04-15 19:39:41 +0300

    Use dev-resources for benchmarks and delete IO-ing benchmarks

--- bench/eximia/bench.clj
+++ bench/eximia/bench.clj
@@ -4,124 +4,107 @@
             clojure.data.xml
             [criterium.core :as cc]
             [clojure.java.io :as io])
-  (:import [java.io StringReader ByteArrayInputStream StringWriter ByteArrayOutputStream]
-           [java.nio.file Files]
-           [java.nio.file.attribute FileAttribute]))
-
-(comment
-  ;; Force the default JRE StAX implementations:
-  (System/setProperty "javax.xml.stream.XMLOutputFactory" "com.sun.xml.internal.stream.XMLOutputFactoryImpl")
-  (System/setProperty "javax.xml.stream.XMLInputFactory" "com.sun.xml.internal.stream.XMLInputFactoryImpl")
-  (System/setProperty "javax.xml.stream.XMLEventFactory" "com.sun.xml.internal.stream.events.XMLEventFactoryImpl"))
+  (:import [java.io StringReader ByteArrayInputStream StringWriter ByteArrayOutputStream]))
 
 ;;;; # Reading
 
-(def pom-file (io/file "pom.xml"))
-(def ^String pom-str (slurp pom-file))
-(def pom-bytes (.getBytes pom-str))
-
 ;;;; ## InputStream
 
-(defn benchmark-pom-io-inputstream []
-  (println "# clojure.xml:\n")
-  (cc/bench
-    (with-open [input (io/input-stream pom-file)]
-      (clojure.xml/parse input)))
+(defn benchmark-inputstream [bytes]
+  (println "### clojure.xml:\n")
+  (cc/quick-bench (clojure.xml/parse (ByteArrayInputStream. bytes)))
 
-  (println "\n# data.xml:\n")
-  (cc/bench
-    (with-open [input (io/input-stream pom-file)]
-      (clojure.data.xml/parse input)))
+  (println "\n### data.xml:\n")
+  (cc/quick-bench (clojure.data.xml/parse (ByteArrayInputStream. bytes)))
 
-  (println "\n# eximia:\n")
-  (cc/bench
-    (with-open [input (io/input-stream pom-file)]
-      (e/read input))))
-
-(defn benchmark-pom-inputstream []
-  (println "# clojure.xml:\n")
-  (cc/bench (clojure.xml/parse (ByteArrayInputStream. pom-bytes)))
-
-  (println "\n# data.xml:\n")
-  (cc/bench (clojure.data.xml/parse (ByteArrayInputStream. pom-bytes)))
-
-  (println "\n# eximia:\n")
-  (cc/bench (e/read (ByteArrayInputStream. pom-bytes))))
+  (println "\n### eximia:\n")
+  (cc/quick-bench (e/read (ByteArrayInputStream. bytes))))
 
 ;;;; ## Reader
 
-(defn benchmark-pom-io-reader []
-  (println "# data.xml:\n")
-  (cc/bench
-    (with-open [input (io/reader pom-file)]
-      (clojure.data.xml/parse input)))
+(defn benchmark-reader [str]
+  (println "### data.xml:\n")
+  (cc/quick-bench (clojure.data.xml/parse (StringReader. str)))
 
-  (println "\n# eximia:\n")
-  (cc/bench
-    (with-open [input (io/reader pom-file)]
-      (e/read input))))
-
-(defn benchmark-pom-reader []
-  (println "# data.xml:\n")
-  (cc/bench (e/read (StringReader. pom-str)))
-
-  (println "\n# eximia:\n")
-  (cc/bench (e/read (StringReader. pom-str))))
+  (println "\n### eximia:\n")
+  (cc/quick-bench (e/read (StringReader. str))))
 
 ;;;; ## String
 
-(defn benchmark-pom-str []
-  (println "# data.xml:\n")
-  (cc/bench (clojure.data.xml/parse-str pom-str))
+(defn benchmark-from-str [str]
+  (println "### data.xml:\n")
+  (cc/quick-bench (clojure.data.xml/parse-str str))
 
-  (println "\n# eximia:\n")
-  (cc/bench (e/read-str pom-str)))
+  (println "\n### eximia:\n")
+  (cc/quick-bench (e/read-str str)))
+
+;;;; ## Run 'Em
+
+(defn benchmark-read []
+  (doseq [filename ["hello.xml", "1.xml", "10.xml", "100.xml"]
+          :let [file (io/file (str "dev-resources/" filename))
+                input-str (slurp file)
+                input-bytes (.getBytes input-str)]]
+    (println "\n#" filename "\n")
+
+    (println "## InputStream\n")
+    (benchmark-inputstream input-bytes)
+
+    (println "\n## Reader\n")
+    (benchmark-reader input-str)
+
+    (println "\n## String\n")
+    (benchmark-from-str input-str)))
 
 ;;;; # Writing
 
-;; HACK: remove namespaces to make data.xml emit work:
-(def data-xml-tree (assoc (clojure.data.xml/parse-str pom-str) :attrs {}))
-(def eximia-tree (assoc (e/read-str pom-str) :attrs {}))
-
-(def temp-file (.toFile (Files/createTempFile nil ".xml" (make-array FileAttribute 0))))
-
 ;;;; ## OutputStream
 
-(defn benchmark-pom-io-outputstream []
-  (println "# eximia:\n")
-  (cc/bench
-    (with-open [output (io/output-stream temp-file)]
-      (e/write eximia-tree output))))
-
-(defn benchmark-pom-outputstream []
-  (println "# eximia:\n")
-  (cc/bench (e/write eximia-tree (ByteArrayOutputStream.))))
+(defn benchmark-outputstream [eximia-tree]
+  (println "### eximia:\n")
+  (cc/quick-bench (e/write eximia-tree (ByteArrayOutputStream.))))
 
 ;;;; ## Writer
 
-(defn benchmark-pom-io-writer []
-  (println "# data.xml:\n")
-  (cc/bench
-    (with-open [output (io/writer temp-file)]
-      (clojure.data.xml/emit data-xml-tree output)))
+(defn benchmark-writer [clojure-xml-tree data-xml-tree eximia-tree]
+  (println "### clojure.xml:\n")
+  (cc/quick-bench
+    (binding [*out* (StringWriter.)]
+      (clojure.xml/emit clojure-xml-tree)))
 
-  (println "\n# eximia:\n")
-  (cc/bench
-    (with-open [output (io/writer temp-file)]
-      (e/write eximia-tree output))))
+  (println "\n### data.xml:\n")
+  (cc/quick-bench (clojure.data.xml/emit data-xml-tree (StringWriter.)))
 
-(defn benchmark-pom-writer []
-  (println "# data.xml:\n")
-  (cc/bench (clojure.data.xml/emit data-xml-tree (StringWriter.)))
-
-  (println "\n# eximia:\n")
-  (cc/bench (e/write eximia-tree (StringWriter.))))
+  (println "\n### eximia:\n")
+  (cc/quick-bench (e/write eximia-tree (StringWriter.))))
 
 ;;;; ## String
 
-(defn benchmark-pom-to-str []
-  (println "# data.xml:\n")
-  (cc/bench (clojure.data.xml/emit-str data-xml-tree))
+(defn benchmark-to-str [data-xml-tree eximia-tree]
+  (println "### data.xml:\n")
+  (cc/quick-bench (clojure.data.xml/emit-str data-xml-tree))
 
-  (println "\n# eximia:\n")
-  (cc/bench (e/write-str eximia-tree)))
+  (println "\n### eximia:\n")
+  (cc/quick-bench (e/write-str eximia-tree)))
+
+;;;; ## Run 'Em
+
+(defn benchmark-write []
+  (doseq [filename ["hello.xml", "1.xml", "10.xml", "100.xml"]
+          :let [file (io/file (str "dev-resources/" filename))
+                ^String input (slurp file)
+                ;; HACK: remove namespaces to make data.xml emit work:
+                clojure-xml-tree (assoc (clojure.xml/parse (ByteArrayInputStream. (.getBytes input))) :attrs {})
+                data-xml-tree (assoc (clojure.data.xml/parse-str input) :attrs {})
+                eximia-tree (assoc (e/read-str input) :attrs {})]]
+
+    (println "\n#" filename "\n")
+
+    (println "## OutputStream\n")
+    (benchmark-outputstream eximia-tree)
+
+    (println "\n## Writer\n")
+    (benchmark-writer clojure-xml-tree data-xml-tree eximia-tree)
+
+    (println "\n## String\n")
+    (benchmark-to-str data-xml-tree eximia-tree)))
